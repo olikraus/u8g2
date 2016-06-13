5648c5ea2a97f4623618ab2b50a613489727a149
olikraus <olikraus@gmail.com>
2016-06-13 22:04:45 +0200

    input value (not yet finished)

--- csrc/u8g2_input_value.c
+++ csrc/u8g2_input_value.c
@@ -41,54 +41,72 @@
     1: value has been updated
 */
 
-uint8_t u8x8_UserInterfaceInputValue(u8x8_t *u8x8, const char *title, const char *pre, uint8_t *value, uint8_t lo, uint8_t hi, uint8_t digits, const char *post)
+uint8_t u8g2_UserInterfaceInputValue(u8g2_t *u8g2, const char *title, const char *pre, uint8_t *value, uint8_t lo, uint8_t hi, uint8_t digits, const char *post)
 {
+  uint8_t line_height;
   uint8_t height;
-  uint8_t y;
-  uint8_t width;
-  uint8_t x;
+  u8g2_uint_t pixel_height;
+  u8g2_uint_t  y, yy;
+  u8g2_uint_t  pixel_width;
+  u8g2_uint_t  x, xx;
+  
   uint8_t local_value = *value;
   uint8_t r;
   uint8_t event;
 
+  /* only horizontal strings are supported, so force this here */
+  u8g2_SetFontDirection(u8g2, 0);
+
+  /* force baseline position */
+  u8g2_SetFontPosBaseline(u8g2);
+  
+  /* calculate line height */
+  line_height = u8g2_GetAscent(u8g2);
+  line_height -= u8g2_GetDescent(u8g2);
+  
+  
   /* calculate overall height of the input value box */
   height = 1;	/* value input line */
   height += u8x8_GetStringLineCnt(title);
-  
+
+  /* calculate the height in pixel */
+  pixel_height = height;
+  pixel_height *= line_height;
+
+
   /* calculate offset from top */
   y = 0;
-  if ( height < u8g2_GetDisplayHeight(u8g2)  )
+  if ( pixel_height < u8g2_GetDisplayHeight(u8g2)  )
   {
     y = u8g2_GetDisplayHeight(u8g2);
-    y -= height;
+    y -= pixel_height;
     y /= 2;
   }
   
   /* calculate offset from left for the label */
   x = 0;
-  width = u8x8_GetUTF8Len(u8x8, pre);
-  width += digits;
-  width += u8x8_GetUTF8Len(u8x8, post);
-  if ( width < u8x8_GetCols(u8x8) )
+  pixel_width = u8g2_GetUTF8Width(u8g2, pre);
+  pixel_width += u8g2_GetUTF8Width(u8g2, "0") * digits;
+  pixel_width += u8g2_GetUTF8Width(u8g2, post);
+  if ( pixel_width < u8g2_GetDisplayWidth(u8g2) )
   {
-    x = u8x8_GetCols(u8x8);
-    x -= width;
+    x = u8g2_GetDisplayWidth(u8g2);
+    x -= pixel_width;
     x /= 2;
   }
   
   /* render */
-  u8x8_ClearDisplay(u8x8);   /* required, because not everything is filled */
-  u8x8_SetInverseFont(u8x8, 0);  
-  y += u8x8_DrawUTF8Lines(u8x8, 0, y, u8x8_GetCols(u8x8), title);
-  x += u8x8_DrawUTF8(u8x8, x, y, pre);
-  u8x8_DrawUTF8(u8x8, x+digits, y, post);
-  u8x8_SetInverseFont(u8x8, 1);
+  yy = y;
+  yy += u8g2_DrawUTF8Lines(u8g2, 0, yy, u8g2_GetDisplayWidth(u8g2), line_height, title);
+  xx = x;
+  xx += u8g2_DrawUTF8(u8g2, xx, yy, pre);
+  xx += u8g2_DrawUTF8(u8g2, xx, yy, u8x8_u8toa(local_value, digits));
+  u8g2_DrawUTF8(u8g2, xx, yy, post);
   
   /* event loop */
-  u8x8_DrawUTF8(u8x8, x, y, u8x8_u8toa(local_value, digits));
   for(;;)
   {
-    event = u8x8_GetMenuEvent(u8x8);
+    event = u8x8_GetMenuEvent(u8g2_GetU8x8(u8g2));
     if ( event == U8X8_MSG_GPIO_MENU_SELECT )
     {
       *value = local_value;
@@ -106,7 +124,6 @@ uint8_t u8x8_UserInterfaceInputValue(u8x8_t *u8x8, const char *title, const char
 	local_value = lo;
       else
 	local_value++;
-      u8x8_DrawUTF8(u8x8, x, y, u8x8_u8toa(local_value, digits));
     }
     else if ( event == U8X8_MSG_GPIO_MENU_PREV || event == U8X8_MSG_GPIO_MENU_DOWN )
     {
@@ -114,10 +131,8 @@ uint8_t u8x8_UserInterfaceInputValue(u8x8_t *u8x8, const char *title, const char
 	local_value = hi;
       else
 	local_value--;
-      u8x8_DrawUTF8(u8x8, x, y, u8x8_u8toa(local_value, digits));
     }        
   }
   
-  u8x8_SetInverseFont(u8x8, 0);
   return r;  
 }
