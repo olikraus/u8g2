ad75fc8ee517db97ce22cf9f4950dd245c17939a
olikraus <olikraus@gmail.com>
2016-06-17 19:42:10 +0200

    ArduBoyTest, started LittleRookChess

--- sys/arduino/u8x8/ArduboyTest/ArduboyTest.ino
+++ sys/arduino/u8x8/ArduboyTest/ArduboyTest.ino
@@ -71,26 +71,94 @@ U8X8_SSD1306_128X64_NONAME_4W_HW_SPI u8x8(/* cs=*/ 12, /* dc=*/ 4, /* reset=*/ 6
 // End of constructor list
 
 ArduboyCore arduboyCore;
-
+ArduboyAudio arduboyAudio;
+ArduboyTunes arduboyTunes;
+  
 void setup(void)
 {
   //u8x8.begin(/*Select=*/ A0, /*Right/Next=*/ 5, /*Left/Prev=*/ 9, /*Up=*/ 8, /*Down=*/ 10, /*Home/Cancel=*/ A1); // Arduboy DevKit
   u8x8.begin(/*Select=*/ 7, /*Right/Next=*/ A1, /*Left/Prev=*/ A2, /*Up=*/ A0, /*Down=*/ A3, /*Home/Cancel=*/ 8); // Arduboy 10 (Production)
+  
+  arduboyTunes.initChannel(PIN_SPEAKER_1);
+  arduboyTunes.initChannel(PIN_SPEAKER_2);
+  arduboyAudio.begin();
+}
+
+void msg(const char *txt)
+{
+  u8x8.clear();
+  u8x8.print(txt);
 }
 
 void loop(void)
 {
   static uint8_t c = 1;
+  static uint8_t b = 0;
   u8x8.setFont(u8x8_font_chroma48medium8_r);
-  c = u8x8.userInterfaceSelectionList("Arduboy Test", c, "Red LEDs\nBlue LEDs");
-  if ( c == 2 )
+  c = u8x8.userInterfaceSelectionList("Arduboy Test\n", c, "LED Test\nButton Test\nSpeaker Test");
+  if ( c == 1 )
   {
+    msg("LED Test");
     arduboyCore.setRGBled(255, 0, 0);
     delay(500);
-    arduboyCore.setRGBled(0, 255, 0);		// green is not there ???
+    arduboyCore.setRGBled(0, 255, 0);    // green is not there ???
     delay(500);
     arduboyCore.setRGBled(0, 0, 255);
     delay(500);
     arduboyCore.setRGBled(0, 0, 0);
+  }
+  else if ( c == 2 )
+  {
+    u8x8.clear();
+    for(;;)
+    {
+      u8x8.drawUTF8(0, 0, "Button Test");
+      b = arduboyCore.buttonsState();
+      
+      u8x8.noInverse();
+      if ( b & A_BUTTON )
+        u8x8.inverse();
+      u8x8.drawUTF8(0, 1, "A Button");
+      
+      u8x8.noInverse();
+      if ( b & B_BUTTON )
+        u8x8.inverse();
+      u8x8.drawUTF8(0, 2, "B Button");
+
+      u8x8.noInverse();
+      if ( b & UP_BUTTON )
+        u8x8.inverse();
+      u8x8.drawUTF8(0, 3, "UP Button");
+
+      u8x8.noInverse();
+      if ( b & DOWN_BUTTON )
+        u8x8.inverse();
+      u8x8.drawUTF8(0, 4, "DOWN Button");
+
+      u8x8.noInverse();
+      if ( b & LEFT_BUTTON )
+        u8x8.inverse();
+      u8x8.drawUTF8(0, 5, "LEFT Button");
+
+      u8x8.noInverse();
+      if ( b & RIGHT_BUTTON )
+        u8x8.inverse();
+      u8x8.drawUTF8(0, 6, "RIGHT Button");
+
+      u8x8.noInverse();
+      u8x8.drawUTF8(0, 7, "Quit: A&B Button");
+
+      if ( (b & A_BUTTON) &&  (b & B_BUTTON) )
+        break;
+    }
+  }
+  else if ( c == 3 )
+  {
+    u8x8.clear();
+    u8x8.drawUTF8(0, 0, "Speaker Test");
+    arduboyTunes.tone(1000, 1000);
+    delay(1000);
+    arduboyTunes.tone(2000, 1000);
+    delay(1000);
   }
 }
