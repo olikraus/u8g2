f5b0b157a49dc882f727e34d55467e6b425129b1
olikraus <olikraus@gmail.com>
2016-06-18 22:44:26 +0200

    update LittleRookChess

--- sys/arduino/u8g2_page_buffer/LittleRookChess/LittleRookChess.ino
+++ sys/arduino/u8g2_page_buffer/LittleRookChess/LittleRookChess.ino
@@ -2,7 +2,7 @@
 
   LittleRookChess.ino
   
-  A Simple Chess Engine (ported from U8glib to U8g2)
+  A Simple Chess Engine (ported from U8glib)
 
   Universal 8bit Graphics Library (https://github.com/olikraus/u8g2/)
 
@@ -118,35 +118,9 @@
   This is a page buffer example.    
 */
 
-// Please UNCOMMENT one of the contructor lines below
-// U8g2 Contructor List (Picture Loop Page Buffer)
-// The complete list is available here: https://github.com/olikraus/u8g2/wiki/u8g2setupcpp
-// Please update the pin numbers according to your setup. Use U8X8_PIN_NONE if the reset pin is not connected
-//U8G2_SSD1306_128X64_NONAME_1_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);
+// Only Arduboy Constructor is here, however other displays are also possible, see here: https://github.com/olikraus/u8g2/wiki/u8g2setupcpp
 U8G2_SSD1306_128X64_NONAME_1_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 12, /* dc=*/ 4, /* reset=*/ 6);	// Arduboy (Production, Kickstarter Edition)
-//U8G2_SSD1306_128X64_NONAME_1_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);
-//U8G2_SSD1306_128X64_NONAME_1_3W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* cs=*/ 10, /* reset=*/ 8);
-//U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* reset=*/ 8);
-//U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // All Boards without Reset of the Display
-//U8G2_SSD1306_128X64_NONAME_1_6800 u8g2(U8G2_R0, 13, 11, 2, 3, 4, 5, 6, A4, /*enable=*/ 7, /*cs=*/ 10, /*dc=*/ 9, /*reset=*/ 8);
-//U8G2_SSD1306_128X64_NONAME_1_8080 u8g2(U8G2_R0, 13, 11, 2, 3, 4, 5, 6, A4, /*enable=*/ 7, /*cs=*/ 10, /*dc=*/ 9, /*reset=*/ 8);
-//U8G2_SSD1306_128X32_UNIVISION_1_SW_I2C u8g2(U8G2_R0, /* clock=*/ 21, /* data=*/ 20, /* reset=*/ U8X8_PIN_NONE);   // Adafruit Feather M0 Basic Proto + FeatherWing OLED
-//U8G2_SSD1306_128X32_UNIVISION_1_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // Adafruit Feather ESP8266/32u4 Boards + FeatherWing OLED
-//U8G2_SSD1306_128X32_UNIVISION_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);   // Adafruit ESP8266/32u4/ARM Boards + FeatherWing OLED
-//U8G2_UC1701_EA_DOGS102_1_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);
-//U8G2_UC1701_EA_DOGS102_1_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);
-//U8G2_ST7920_192X32_1_8080 u8g2(U8G2_R0, 8, 9, 10, 11, 4, 5, 6, 7, /*enable=*/ 18, /*cs=*/ U8X8_PIN_NONE, /*dc=*/ 17, /*reset=*/ U8X8_PIN_NONE);
-//U8G2_ST7920_192X32_1_SW_SPI u8g2(U8G2_R0, /* clock=*/ 18 /* A4 */ , /* data=*/ 16 /* A2 */, /* CS=*/ 17 /* A3 */, /* reset=*/ U8X8_PIN_NONE);
-//U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0, /* clock=*/ 18 /* A4 */ , /* data=*/ 16 /* A2 */, /* CS=*/ 17 /* A3 */, /* reset=*/ U8X8_PIN_NONE);
-//U8G2_ST7565_EA_DOGM128_1_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);
-//U8G2_ST7565_EA_DOGM128_1_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);
-//U8G2_ST7565_NHD_C12832_1_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);
-//U8G2_ST7565_NHD_C12832_1_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);
 
-
-
-
-// End of constructor list
 
 void chess_Init(u8g2_t *u8g, uint8_t body_color);
 
@@ -1822,6 +1796,21 @@ void chess_ComputerMove(uint8_t depth)
 /* display menu */
 /*==============================================================*/
 
+extern const uint8_t chess_pieces_body_bm[] CHESS_PROGMEM; // forward decl
+extern const uint8_t chess_black_pieces_bm[] CHESS_PROGMEM; // forward decl 
+
+// uint8_t chess_key_code = 0;  // obsolete, u8g2 does proper debouncing
+uint8_t chess_key_cmd = 0;
+#define CHESS_STATE_MENU 0
+#define CHESS_STATE_SELECT_START 1
+#define CHESS_STATE_SELECT_PIECE 2
+#define CHESS_STATE_SELECT_TARGET_POS 3
+#define CHESS_STATE_THINKING 4
+#define CHESS_STATE_GAME_END 5
+uint8_t chess_state = CHESS_STATE_MENU;
+uint8_t chess_source_pos = 255;
+uint8_t chess_target_pos = 255;
+
 #define MNU_FONT u8g2_font_5x8_tr
 #define MNU_ENTRY_HEIGHT 9
 
@@ -1838,10 +1827,15 @@ void mnu_DrawHome(uint8_t is_highlight)
   
   u8g2_SetFont(lrc_u8g, u8g2_font_5x7_tr);
   u8g2_SetDefaultForegroundColor(lrc_u8g);
-  t = u8g2_DrawStrP(lrc_u8g, x, y -1, CHESS_PSTR("Options"));
-    
-  if ( is_highlight )
-    u8g2_DrawFrame(lrc_u8g, x-1, y - MNU_ENTRY_HEIGHT +1, t, MNU_ENTRY_HEIGHT);  
+  if ( chess_state == CHESS_STATE_THINKING )
+     u8g2_DrawBitmap(lrc_u8g, x, y-8, 1, 8, chess_black_pieces_bm+24);
+  else
+  {     
+    t = u8g2_DrawStrP(lrc_u8g, x, y -1, CHESS_PSTR("Options"));
+      
+    if ( is_highlight )
+      u8g2_DrawFrame(lrc_u8g, x-1, y - MNU_ENTRY_HEIGHT +1, t, MNU_ENTRY_HEIGHT);  
+  }
 }
 
 void mnu_DrawEntry(uint8_t y, const char *str, uint8_t is_clr_background, uint8_t is_highlight)
@@ -1892,6 +1886,10 @@ void mnu_Draw(void)
   
   y -= MNU_ENTRY_HEIGHT;
   
+
+  u8g2_DrawBitmap(lrc_u8g, 0, 1, 1, 8, chess_black_pieces_bm+24);
+  u8g2_DrawBitmap(lrc_u8g, 128-8, 1, 1, 8, chess_black_pieces_bm+24);
+
   
   for( i = 0; i < mnu_max; i++ )
   {
@@ -1917,17 +1915,8 @@ void mnu_Step(uint8_t key_cmd)
 
 
 
-// uint8_t chess_key_code = 0;  // obsolete, u8g2 does proper debouncing
-uint8_t chess_key_cmd = 0;
-#define CHESS_STATE_MENU 0
-#define CHESS_STATE_SELECT_START 1
-#define CHESS_STATE_SELECT_PIECE 2
-#define CHESS_STATE_SELECT_TARGET_POS 3
-#define CHESS_STATE_THINKING 4
-#define CHESS_STATE_GAME_END 5
-uint8_t chess_state = CHESS_STATE_MENU;
-uint8_t chess_source_pos = 255;
-uint8_t chess_target_pos = 255;
+
+
 
 const uint8_t chess_pieces_body_bm[] CHESS_PROGMEM = 
 {
