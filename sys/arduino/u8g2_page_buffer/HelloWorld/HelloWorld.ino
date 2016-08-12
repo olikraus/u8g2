09650cbf98e16d76f9e6e41fa32fc252367e9309
olikraus <olikraus@gmail.com>
2016-08-12 21:23:19 +0200

    2.2.2 tmp release

--- sys/arduino/u8g2_page_buffer/HelloWorld/HelloWorld.ino
+++ sys/arduino/u8g2_page_buffer/HelloWorld/HelloWorld.ino
@@ -96,10 +96,10 @@ U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11,
 void setup(void) {
 
   /* U8g2 Project: SSD1306 Test Board */
-  pinMode(10, OUTPUT);
-  pinMode(9, OUTPUT);
-  digitalWrite(10, 0);
-  digitalWrite(9, 0);		
+  //pinMode(10, OUTPUT);
+  //pinMode(9, OUTPUT);
+  //digitalWrite(10, 0);
+  //digitalWrite(9, 0);		
 
   /* U8g2 Project: T6963 Test Board */
   //pinMode(18, OUTPUT);
