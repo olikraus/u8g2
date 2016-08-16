76a930d248d160b068b00bc24193da73e2daeb30
Pal Denes <pdenes@codingafter9.com>
2016-08-16 21:38:56 +0100

    Add support for MINI12864 (UC1701)
    Ported the driver from u8glib, almost the same as DOGS102 (UC1701), just
    with a different size. Tested with a display salvaged from a cheap ebay
    MP3 player.

--- csrc/u8x8.h
+++ csrc/u8x8.h
@@ -670,6 +670,7 @@ void utf8_show(void);		/* show content of UTF-8 frame buffer */
 /*==========================================*/
 /* u8x8_d_XXX.c */
 uint8_t u8x8_d_uc1701_ea_dogs102(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
+uint8_t u8x8_d_uc1701_mini12864(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
 uint8_t u8x8_d_ssd1306_128x64_noname(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
 uint8_t u8x8_d_sh1106_128x64_noname(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
 uint8_t u8x8_d_st7920_192x32(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
