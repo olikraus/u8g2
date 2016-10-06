adc77b44889e0ae5dfd787eb2bf60884f4bf601b
olikraus <olikraus@gmail.com>
2016-10-06 21:31:23 +0200

    fixed issue #67

--- sys/arduino/u8g2_full_buffer/HelloWorld/HelloWorld.ino
+++ sys/arduino/u8g2_full_buffer/HelloWorld/HelloWorld.ino
@@ -99,7 +99,7 @@ void setup(void) {
 }
 
 void loop(void) {
-  u8g2.clearBuffer();					// clear the internal menory
+  u8g2.clearBuffer();					// clear the internal memory
   u8g2.setFont(u8g2_font_ncenB14_tr);	// choose a suitable font
   u8g2.drawStr(0,20,"Hello World!");	// write something to the internal memory
   u8g2.sendBuffer();					// transfer internal memory to the display
