a921a9bfbd31cbe7621dec5eab46542e6a5bce21
olikraus <olikraus@gmail.com>
2016-11-13 18:24:27 +0100

    dogxl240 spi

--- sys/arduino/u8g2_full_buffer/SelectionList/SelectionList.ino
+++ sys/arduino/u8g2_full_buffer/SelectionList/SelectionList.ino
@@ -93,6 +93,7 @@
 //U8G2_UC1610_EA_DOGXL160_F_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/  U8X8_PIN_NONE);
 //U8G2_UC1611_EA_DOGM240_F_2ND_HW_I2C u8g2(U8G2_R0, /* reset=*/ 8);	// Due, 2nd I2C, DOGM240 Test Board
 //U8G2_UC1611_EA_DOGXL240_F_2ND_HW_I2C u8g2(U8G2_R0, /* reset=*/ 8);	// Due, 2nd I2C, DOGXL240 Test Board
+//U8G2_UC1611_EA_DOGXL240_F_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);   // Due, SW SPI, DOGXL240 Test Board
 
 
 // End of constructor list
