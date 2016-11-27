e6f42400e7f7b82fa2ac6b9a8666a4ec8448b56d
olikraus <olikraus@gmail.com>
2016-11-27 14:16:53 +0100

    update

--- csrc/u8g2.h
+++ csrc/u8g2.h
@@ -586,7 +586,7 @@ void u8g2_Setup_a2printer_384x240_f(u8g2_t *u8g2, const u8g2_cb_t *rotation, u8x
 void u8g2_SendBuffer(u8g2_t *u8g2);
 void u8g2_ClearBuffer(u8g2_t *u8g2);
 
-void u8g2_SetBufferCurrTileRow(u8g2_t *u8g2, uint8_t row) U8G2_NOINLINE;
+void u8g2_SetPageCurrTileRow(u8g2_t *u8g2, uint8_t row) U8G2_NOINLINE;
 void u8g2_FirstPage(u8g2_t *u8g2);
 uint8_t u8g2_NextPage(u8g2_t *u8g2);
 
@@ -594,7 +594,7 @@ uint8_t u8g2_NextPage(u8g2_t *u8g2);
 #define u8g2_GetBufferTileHeight(u8g2)	((u8g2)->tile_buf_height)
 #define u8g2_GetBufferTileWidth(u8g2)	(u8g2_GetU8x8(u8g2)->display_info->tile_width)
 /* the following variable is only valid after calling u8g2_FirstPage */
-#define u8g2_GetBufferCurrTileRow(u8g2) ((u8g2)->tile_curr_row)
+#define u8g2_GetPageCurrTileRow(u8g2) ((u8g2)->tile_curr_row)
 
 /*==========================================*/
 /* u8g2_ll_hvline.c */
