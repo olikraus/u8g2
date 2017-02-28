2d1d643d43c0113c531133dc0a75d506a31f77da
olikraus <olikraus@gmail.com>
2017-02-28 21:14:44 +0100

    removed uc1617

--- tools/codebuild/codebuild.c
+++ tools/codebuild/codebuild.c
@@ -411,7 +411,7 @@ struct controller controller_list[] =
       { NULL }
     }
   },  
-
+#ifdef NOTUSED
   {
     "uc1617", 	16, 	16, 	"u8g2_ll_hvline_vertical_top_lsb", "u8x8_cad_001", "", COM_4WSPI|COM_3WSPI,
     "", /* is_generate_u8g2_class= */ 1,
@@ -428,6 +428,7 @@ struct controller controller_list[] =
       { NULL }
     }
   },  
+#endif
   
   {
     "st7565", 		16, 	8, 	"u8g2_ll_hvline_vertical_top_lsb", "u8x8_cad_001", "", COM_4WSPI|COM_3WSPI|COM_6800|COM_8080,
