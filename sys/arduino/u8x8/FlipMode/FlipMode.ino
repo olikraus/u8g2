47b6a0126c822d009cfb84e66d37869fcd7e84ee
olikraus <olikraus@gmail.com>
2017-03-10 05:50:31 +0100

    update

--- sys/arduino/u8x8/FlipMode/FlipMode.ino
+++ sys/arduino/u8x8/FlipMode/FlipMode.ino
@@ -120,7 +120,6 @@
 //U8X8_SSD1606_172X72_4W_SW_SPI u8x8(/* clock=*/ 13, /* data=*/ 11, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);	// eInk/ePaper Display
 //U8X8_SSD1607_200X200_4W_SW_SPI u8x8(/* clock=*/ 13, /* data=*/ 11, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);	// eInk/ePaper Display
 
-U8X8_ST75256_JLX256128_4W_SW_SPI u8x8(/* clock=*/ 13, /* data=*/ 11, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);	
 
 
 // End of constructor list
