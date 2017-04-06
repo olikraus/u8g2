f9799559d089f76b50d3494ca00385b50f060781
olikraus <olikraus@gmail.com>
2017-04-06 22:38:35 +0200

    update

--- sys/arm/stm32l031x6/blink/main.c
+++ sys/arm/stm32l031x6/blink/main.c
@@ -1,5 +1,4 @@
-// stm32flash -e 255 -w blink.hex -v /dev/ttyUSB0
-
+/* LED blink project for the STM32L031 */
 
 #include "stm32l031xx.h"
 
@@ -7,41 +6,29 @@ volatile unsigned long SysTickCount = 0;
 
 void __attribute__ ((interrupt, used)) SysTick_Handler(void)
 {
-  SysTickCount++;
-  
+  SysTickCount++;  
   if ( SysTickCount & 1 )
     GPIOA->BSRR = GPIO_BSRR_BS_13;		/* atomic set PA13 */
   else
     GPIOA->BSRR = GPIO_BSRR_BR_13;		/* atomic clr PA13 */
-
 }
-
-
 
 int main()
 {
-
-  //RCC->CFGR &= 
   RCC->IOPENR |= RCC_IOPENR_IOPAEN;		/* Enable clock for GPIO Port A */
   __NOP();
   __NOP();
-
-  
-  
   GPIOA->MODER &= ~GPIO_MODER_MODE13;	/* clear mode for PA13 */
   GPIOA->MODER |= GPIO_MODER_MODE13_0;	/* Output mode for PA13 */
   GPIOA->OTYPER &= ~GPIO_OTYPER_OT_13;	/* no Push/Pull for PA13 */
   GPIOA->OSPEEDR &= ~GPIO_OSPEEDER_OSPEED13;	/* low speed for PA13 */
   GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD13;	/* no pullup/pulldown for PA13 */
-  //GPIOA->BSRR = GPIO_BSRR_BS_13;		/* atomic set PA13 */
   GPIOA->BSRR = GPIO_BSRR_BR_13;		/* atomic clr PA13 */
 
-  //SysTick->LOAD = (SystemCoreClock/1000UL*(unsigned long)SYS_TICK_PERIOD_IN_MS) - 1;
   SysTick->LOAD = 2000*500 - 1;
   SysTick->VAL = 0;
   SysTick->CTRL = 7;   /* enable, generate interrupt (SysTick_Handler), do not divide by 2 */
-  
-  
+    
   for(;;)
     ;
 }
