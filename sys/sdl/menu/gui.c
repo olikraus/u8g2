dad9a687a147387235c465fdea6ca9a4ed5ab724
olikraus <olikraus@gmail.com>
2017-04-26 21:51:42 +0200

    update

--- sys/sdl/menu/gui.c
+++ sys/sdl/menu/gui.c
@@ -36,7 +36,7 @@ menu_t gui_menu;
 
 /*============================================*/
 
-void gui_alarm_calc_next_alarm(uint8_t idx, uint16_t current_week_time_in_minutes)
+void gui_alarm_calc_next_wd_alarm(uint8_t idx, uint16_t current_week_time_in_minutes)
 {
   uint8_t i;
   uint16_t week_time_abs;
@@ -45,22 +45,32 @@ void gui_alarm_calc_next_alarm(uint8_t idx, uint16_t current_week_time_in_minute
   gui_alarm_list[idx].na_week_time_in_minutes = 0x0ffff;		/* not found */
   gui_alarm_list[idx].na_minutes_diff = 0x0ffff;				/* not found */
   gui_alarm_list[idx].na_wd = 7;						/* not found */
+  
+  printf("gui_alarm_calc_next_wd_alarm: %d\n", idx);
+  
   if ( gui_alarm_list[idx].enable != 0 )
   {
+    printf("gui_alarm_calc_next_wd_alarm: %d enabled\n", idx);
     for( i = 0; i < 7; i++ )
     {
       if ( gui_alarm_list[idx].wd[i] != 0 )
       {
 	if ( gui_alarm_list[idx].skip_wd != i+1 )
 	{
-	  week_time_abs = 24*i + gui_alarm_list[idx].h*60 + gui_alarm_list[idx].m;
-	  week_time_abs += gui_alarm_list[idx].snooze_count*SNOOZE_MINUTES;
+	  week_time_abs = i; 
+	  week_time_abs *= 24; 
+	  week_time_abs += gui_alarm_list[idx].h;
+	  week_time_abs *= 60;
+	  week_time_abs += gui_alarm_list[idx].m;
+	  week_time_abs += gui_alarm_list[idx].snooze_count*(uint16_t)SNOOZE_MINUTES;
 	  
 	  if ( current_week_time_in_minutes <= week_time_abs )
 	    week_time_diff = week_time_abs - current_week_time_in_minutes;
 	  else
 	    week_time_diff = week_time_abs + 7*24*60 - current_week_time_in_minutes;
-	    
+
+	  printf("gui_alarm_calc_next_wd_alarm: %d week_time_abs=%d current_week_time_in_minutes=%d week_time_diff=%d\n", idx, week_time_abs, current_week_time_in_minutes,week_time_diff);
+	  
 	  if (  best_diff > week_time_diff )
 	  {
 	    best_diff = week_time_diff;
@@ -75,6 +85,8 @@ void gui_alarm_calc_next_alarm(uint8_t idx, uint16_t current_week_time_in_minute
       }
     }
   }
+  printf("gui_alarm_calc_next_wd_alarm: %d na_minutes_diff=%d\n", idx, gui_alarm_list[idx].na_minutes_diff);
+  printf("gui_alarm_calc_next_wd_alarm: %d na_wd=%d\n", idx, gui_alarm_list[idx].na_wd);
 }
 
 
@@ -113,7 +125,7 @@ void gui_date_adjust(void)
     /* adjust the weekday so that 0 will be Monday */
     gui_data.weekday += 6;
     if ( gui_data.weekday >= 7 ) 
-      gui_data.weekday -= 6;
+      gui_data.weekday -= 7;
     //cdn = to_century_day_number(y, ydn);
     //to_minutes(cdn, h, m);
 }
@@ -124,7 +136,12 @@ void gui_date_adjust(void)
 */
 void gui_calc_week_time(void)
 {
-  gui_data.week_time = gui_data.weekday*24 + gui_data.h * 60 + gui_data.mt * 10 + gui_data.mo;
+  gui_data.week_time = gui_data.weekday;
+  gui_data.week_time  *= 24;
+  gui_data.week_time += gui_data.h;
+  gui_data.week_time  *= 60;
+  gui_data.week_time  += gui_data.mt * 10 + gui_data.mo;
+  
 }
 
 /*
@@ -139,22 +156,25 @@ void gui_calc_next_alarm(void)
   /* step 1: Calculate the difference to current weektime for each alarm */
   /* result is stored in gui_alarm_list[i].na_minutes_diff */
   for( i = 0; i < GUI_ALARM_CNT; i++ )
-    gui_alarm_calc_next_alarm(i, gui_data.week_time);
+    gui_alarm_calc_next_wd_alarm(i, gui_data.week_time);
   
   /* step 2: find the index with the lowest difference */
   lowest_diff = 0x0ffff;
   lowest_i = GUI_ALARM_CNT;
   for( i = 0; i < GUI_ALARM_CNT; i++ )
   {
-    if ( lowest_diff < gui_alarm_list[i].na_minutes_diff )
+    if ( lowest_diff > gui_alarm_list[i].na_minutes_diff )
     {
       lowest_diff = gui_alarm_list[i].na_minutes_diff;
       lowest_i = i;
     }
   }
   
+  
   /* step 3: store the result */
   gui_data.next_alarm_index = lowest_i;  /* this can be GUI_ALARM_CNT */
+  printf("gui_calc_next_alarm gui_data.next_alarm_index=%d\n", gui_data.next_alarm_index);
+  
   gui_data.is_skip_possible = 0;
   if ( lowest_i < GUI_ALARM_CNT )
   {
@@ -256,7 +276,27 @@ int me_action_handle_display_time(menu_t *menu, const me_t *me, uint8_t msg)
   if ( msg == ME_MSG_DRAW )
   {
     char s[14];
+    u8g2_uint_t x = 4;
     u8g2_SetFont(menu->u8g2, MENU_NORMAL_FONT);
+
+    if ( gui_data.next_alarm_index < GUI_ALARM_CNT )
+    {
+      u8g2_DrawXBM(menu->u8g2, 67, 20, 12, 12, (const uint8_t *)(alarm_xbm));
+      strcpy(s, weekdaystr[gui_alarm_list[gui_data.next_alarm_index].na_wd]);
+      s[2] = ',';
+      s[3] = ' ';
+      strcpy(s+4, u8x8_u8toa(gui_alarm_list[gui_data.next_alarm_index].na_h, 2));
+      s[6] = ':';
+      strcpy(s+7, u8x8_u8toa(gui_alarm_list[gui_data.next_alarm_index].na_m, 2));
+      s[9] = '\0';
+      
+      u8g2_DrawUTF8(menu->u8g2, 81, 30, s);
+      
+    }
+    else
+    {
+      x= 34;
+    }
     
     strcpy(s, weekdaystr[gui_data.weekday]);
     s[2] = ',';
@@ -268,14 +308,8 @@ int me_action_handle_display_time(menu_t *menu, const me_t *me, uint8_t msg)
     s[10] = gui_data.year_t+'0';
     s[11] = gui_data.year_o+'0';
     s[12] = '\0';
-    u8g2_DrawUTF8(menu->u8g2, 10, 30, s);
+    u8g2_DrawUTF8(menu->u8g2, x, 30, s);
     
-    gui_data.next_alarm_index = 0;
-    if ( gui_data.next_alarm_index < GUI_ALARM_CNT )
-    {
-      u8g2_DrawXBM(menu->u8g2, 70, 20, 12, 12, (const uint8_t *)(alarm_xbm));
-      u8g2_DrawUTF8(menu->u8g2, 85, 30, gui_alarm_str[gui_data.next_alarm_index]);
-    }
     
     return 1;
   }
@@ -350,8 +384,6 @@ const me_t melist_setup_alarm[] =
   { me_cb_num_label, NULL, ":", 	ME_ALARM_TIME_XO+30,ME_ALARM_TIME_Y-3 },
   { me_cb_0_55, &(gui_alarm_current.m), NULL, 		ME_ALARM_TIME_XO+39,ME_ALARM_TIME_Y },
   
-  { me_cb_16x16_bitmap_button, (void *)me_action_alarm_done, (void *)ok_xbm, ME_ALARM_TIME_XO+80,ME_ALARM_TIME_Y-17 },
-
   
   { me_cb_wd_toggle, &(gui_alarm_current.wd[0]), (void *)weekdaystr[0], 	ME_ALARM_WD_XO+17*0, ME_ALARM_WD_Y},
   { me_cb_wd_toggle, &(gui_alarm_current.wd[1]), (void *)weekdaystr[1], 	ME_ALARM_WD_XO+17*1, ME_ALARM_WD_Y},
@@ -360,6 +392,9 @@ const me_t melist_setup_alarm[] =
   { me_cb_wd_toggle, &(gui_alarm_current.wd[4]), (void *)weekdaystr[4], 	ME_ALARM_WD_XO+17*4, ME_ALARM_WD_Y},
   { me_cb_wd_toggle, &(gui_alarm_current.wd[5]), (void *)weekdaystr[5], 	ME_ALARM_WD_XO+17*5, ME_ALARM_WD_Y},
   { me_cb_wd_toggle, &(gui_alarm_current.wd[6]), (void *)weekdaystr[6], 	ME_ALARM_WD_XO+17*6, ME_ALARM_WD_Y},
+
+  { me_cb_16x16_bitmap_button, (void *)me_action_alarm_done, (void *)ok_xbm, ME_ALARM_TIME_XO+80,ME_ALARM_TIME_Y-17 },
+
   
   { me_cb_null, NULL, NULL, 0, 0 },
 };
@@ -398,37 +433,6 @@ int me_action_alarm4(menu_t *menu, const me_t *me, uint8_t msg)
   return me_action_alarm_common(menu, me, msg);
 }
 
-const me_t melist_alarm_menu[] = 
-{
-  { me_cb_button_half_line, (void *)me_action_alarm1, gui_alarm_str[0], 0,10 },
-  { me_cb_button_half_line, (void *)me_action_alarm2, gui_alarm_str[1], 64,10 },
-  { me_cb_button_half_line, (void *)me_action_alarm3, gui_alarm_str[2], 0,20 },
-  { me_cb_button_half_line, (void *)me_action_alarm4, gui_alarm_str[3], 64,20 },
-  { me_cb_button_full_line, (void *)me_action_to_top_menu, "Zurück", 40,30 },
-  { me_cb_null, NULL, NULL, 0, 0 },
-};
-
-int me_action_setup_time(menu_t *menu, const me_t *me, uint8_t msg)
-{
-  if ( msg == ME_MSG_SELECT )
-  {
-    menu_SetMEList(menu, melist_setup_time, 0);
-    return 1;
-  }
-  return 0;
-}
-
-int me_action_setup_date(menu_t *menu, const me_t *me, uint8_t msg)
-{
-  if ( msg == ME_MSG_SELECT )
-  {
-    menu_SetMEList(menu, melist_setup_date, 0);
-    return 1;
-  }
-  return 0;
-}
-
-
 int me_action_handle_alarm_list(menu_t *menu, const me_t *me, uint8_t msg)
 {
   if ( msg == ME_MSG_DRAW )
@@ -453,7 +457,7 @@ int me_action_handle_alarm_list(menu_t *menu, const me_t *me, uint8_t msg)
   }
   else if ( msg == ME_MSG_SELECT )
   {
-    menu_SetMEList(menu, melist_setup_menu, 3);
+    menu_SetMEList(menu, melist_alarm_menu, 4);
     return 1;
   }
   return 0;
@@ -474,6 +478,38 @@ int me_action_goto_alarm_list(menu_t *menu, const me_t *me, uint8_t msg)
   }
   return 0;
 }
+
+const me_t melist_alarm_menu[] = 
+{
+  { me_cb_button_half_line, (void *)me_action_alarm1, gui_alarm_str[0], 0,10 },
+  { me_cb_button_half_line, (void *)me_action_alarm2, gui_alarm_str[1], 64,10 },
+  { me_cb_button_half_line, (void *)me_action_alarm3, gui_alarm_str[2], 0,20 },
+  { me_cb_button_half_line, (void *)me_action_alarm4, gui_alarm_str[3], 64,20 },
+  { me_cb_button_half_line, (void *)me_action_goto_alarm_list, "Liste", 0,30 },
+  { me_cb_button_half_line, (void *)me_action_to_top_menu, "Zurück", 64,30 },
+  { me_cb_null, NULL, NULL, 0, 0 },
+};
+
+int me_action_setup_time(menu_t *menu, const me_t *me, uint8_t msg)
+{
+  if ( msg == ME_MSG_SELECT )
+  {
+    menu_SetMEList(menu, melist_setup_time, 0);
+    return 1;
+  }
+  return 0;
+}
+
+int me_action_setup_date(menu_t *menu, const me_t *me, uint8_t msg)
+{
+  if ( msg == ME_MSG_SELECT )
+  {
+    menu_SetMEList(menu, melist_setup_date, 0);
+    return 1;
+  }
+  return 0;
+}
+
 
 const me_t melist_setup_menu[] = 
 {
