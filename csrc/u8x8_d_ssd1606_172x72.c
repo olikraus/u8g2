9290baea91a31fb13f28ea169d49afdf86da637c
olikraus <olikraus@gmail.com>
2017-08-12 12:34:33 +0200

    ssd1606 tests

--- csrc/u8x8_d_ssd1606_172x72.c
+++ csrc/u8x8_d_ssd1606_172x72.c
@@ -169,7 +169,10 @@ static const uint8_t u8x8_d_ssd1606_to_display_seq[] = {
   U8X8_CA(0x22, 0x03),	/* disable clock and charge pump */
   U8X8_DLY(200),		/* this requres about 270ms */
   U8X8_DLY(90),  
- // U8X8_CA(0x10, 0x01),	/* deep sleep mode */
+  
+  //U8X8_CA(0x10, 0x01), /* deep sleep mode */
+  //U8X8_C(0x20), 		/* execute sequence */
+  U8X8_DLY(50),  
   
   U8X8_END_TRANSFER(),             	/* disable chip */
   U8X8_END()             			/* end of sequence */
