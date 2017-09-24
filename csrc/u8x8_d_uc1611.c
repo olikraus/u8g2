056b245dc39b1e855b87fc9d3e4ef27747a0be53
olikraus <olikraus@gmail.com>
2017-09-24 20:59:47 +0200

    uc1611 controller ram architecture update, issue #339, release 2.18.10

--- csrc/u8x8_d_uc1611.c
+++ csrc/u8x8_d_uc1611.c
@@ -412,40 +412,89 @@ static const uint8_t u8x8_d_uc1611_powersave1_seq[] = {
 /* EW50850, 240x160 */
 uint8_t u8x8_d_uc1611_ew50850(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
 {
-  /* call common procedure first and handle messages there */
-  if ( u8x8_d_uc1611_common(u8x8, msg, arg_int, arg_ptr) == 0 )
+  uint8_t x, y, c, i, v, m0, m1;
+  uint8_t *ptr;
+  /* msg not handled, then try here */
+  switch(msg)
   {
-    /* msg not handled, then try here */
-    switch(msg)
-    {
-      case U8X8_MSG_DISPLAY_SETUP_MEMORY:
-	u8x8_d_helper_display_setup_memory(u8x8, &u8x8_uc1611_ew50850_display_info);
-	break;
-      case U8X8_MSG_DISPLAY_INIT:
-	u8x8_d_helper_display_init(u8x8);
-	u8x8_cad_SendSequence(u8x8, u8x8_d_uc1611_ew50850_init_seq);
-	break;
-      case U8X8_MSG_DISPLAY_SET_POWER_SAVE:
-	if ( arg_int == 0 )
-	  u8x8_cad_SendSequence(u8x8, u8x8_d_uc1611_powersave0_seq);
-	else
-	  u8x8_cad_SendSequence(u8x8, u8x8_d_uc1611_powersave1_seq);
-	break;
-      case U8X8_MSG_DISPLAY_SET_FLIP_MODE:
-	if ( arg_int == 0 )
+    case U8X8_MSG_DISPLAY_DRAW_TILE:
+      u8x8_cad_StartTransfer(u8x8);
+    
+      x = ((u8x8_tile_t *)arg_ptr)->x_pos;
+      x *= 8;
+      x += u8x8->x_offset;
+   
+      u8x8_cad_SendCmd(u8x8, 0x000 | ((x&15)));
+      u8x8_cad_SendCmd(u8x8, 0x010 | (x>>4) );
+    
+      y = ((u8x8_tile_t *)arg_ptr)->y_pos;
+      y*=4;
+      for( i = 0; i < 4; i++ )
+      {
+	m0 = 1<<(i*2);
+	m1 = m0;
+	m1 <<= 1;
+	u8x8_cad_SendCmd(u8x8, 0x060 | (y&15));
+	u8x8_cad_SendCmd(u8x8, 0x070 | (y>>4));
+      
+	c = ((u8x8_tile_t *)arg_ptr)->cnt;
+	c *= 8;
+	ptr = ((u8x8_tile_t *)arg_ptr)->tile_ptr;
+	
+	do
 	{
-	  u8x8_cad_SendSequence(u8x8, u8x8_d_uc1611_alt_flip0_seq);
-	  u8x8->x_offset = u8x8->display_info->default_x_offset;
-	}
-	else
-	{
-	  u8x8_cad_SendSequence(u8x8, u8x8_d_uc1611_alt_flip1_seq);
-	  u8x8->x_offset = u8x8->display_info->flipmode_x_offset;
-	}	
-	break;
-      default:
-	return 0;		/* msg unknown */
-    }
+	  while( c > 0 )
+	  {
+	    v = 0;
+	    if ( *ptr & m0 )
+	      v|= 0x0f;
+	    if ( *ptr & m1 )
+	      v|= 0xf0;
+	    u8x8_cad_SendData(u8x8, 1, &v);	/* note: SendData can not handle more than 255 bytes */
+	    c--;
+	  }
+	  arg_int--;
+	} while( arg_int > 0 );
+	
+	y++;
+      }
+      u8x8_cad_EndTransfer(u8x8);
+      break;
+    case U8X8_MSG_DISPLAY_SETUP_MEMORY:
+      u8x8_d_helper_display_setup_memory(u8x8, &u8x8_uc1611_ew50850_display_info);
+      break;
+    case U8X8_MSG_DISPLAY_INIT:
+      u8x8_d_helper_display_init(u8x8);
+      u8x8_cad_SendSequence(u8x8, u8x8_d_uc1611_ew50850_init_seq);
+      break;
+    case U8X8_MSG_DISPLAY_SET_POWER_SAVE:
+      if ( arg_int == 0 )
+	u8x8_cad_SendSequence(u8x8, u8x8_d_uc1611_powersave0_seq);
+      else
+	u8x8_cad_SendSequence(u8x8, u8x8_d_uc1611_powersave1_seq);
+      break;
+    case U8X8_MSG_DISPLAY_SET_FLIP_MODE:
+      if ( arg_int == 0 )
+      {
+	u8x8_cad_SendSequence(u8x8, u8x8_d_uc1611_alt_flip0_seq);
+	u8x8->x_offset = u8x8->display_info->default_x_offset;
+      }
+      else
+      {
+	u8x8_cad_SendSequence(u8x8, u8x8_d_uc1611_alt_flip1_seq);
+	u8x8->x_offset = u8x8->display_info->flipmode_x_offset;
+      }	
+      break;
+#ifdef U8X8_WITH_SET_CONTRAST
+  case U8X8_MSG_DISPLAY_SET_CONTRAST:
+    u8x8_cad_StartTransfer(u8x8);
+    u8x8_cad_SendCmd(u8x8, 0x081 );
+    u8x8_cad_SendArg(u8x8, arg_int  );	/* uc1611 has range from 0 to 255 */
+    u8x8_cad_EndTransfer(u8x8);
+    break;
+#endif
+    default:
+      return 0;		/* msg unknown */
   }
   return 1;
 }
