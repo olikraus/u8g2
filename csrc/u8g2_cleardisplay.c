830e6aa5cd86843712950653114512ff561892c6
olikraus <olikraus@gmail.com>
2017-09-30 08:28:13 +0200

    bugfix for #370 (reset curr tile row to 0 after clear display)

--- csrc/u8g2_cleardisplay.c
+++ csrc/u8g2_cleardisplay.c
@@ -41,5 +41,14 @@ void u8g2_ClearDisplay(u8g2_t *u8g2)
   u8g2_FirstPage(u8g2);
   do {
   } while ( u8g2_NextPage(u8g2) );
+  /* 
+    This function is usually called during startup (u8g2.begin()).
+    However the user might want to use full buffer mode with clear and 
+    send commands.
+    This will not work because the current tile row is modified by the picture 
+    loop above. To fix this, reset the tile row to 0, issue #370
+    A workaround would be, that the user sets the current tile row to 0 manually.
+  */
+  u8g2_SetBufferCurrTileRow(u8g2, 0);  
 }
 
