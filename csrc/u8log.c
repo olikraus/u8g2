015166d26100f250a5361711025daf6950ace5e2
kraus <olikraus@gmail.com>
2018-04-15 18:34:49 +0200

    update

--- csrc/u8log.c
+++ csrc/u8log.c
@@ -11,22 +11,30 @@
 typedef struct u8log_struct u8log_t;
 
 
-/* redraw the specified line. If line == 255 then redraw everything */
+/* redraw the specified line. */
 typedef void (*u8log_cb)(u8log_t * u8log, uint8_t line);
 
 struct u8log_struct
 {
+  /* configuration */
   void *aux_data;		/* pointer to u8x8 or u8g2 */
+  uint8_t width, height;	/* size of the terminal */
   u8log_cb cb;			/* callback redraw function */
   uint8_t *screen_buffer;	/* size must be width*heigh bytes */
-  uint8_t width, height;	/* size of the terminal */
-  uint8_t last_x, last_y;	/* position of the last printed char */
+  uint8_t is_redraw_line_for_each_char;
+  
+  
+  /* internal data */
+  //uint8_t last_x, last_y;	/* position of the last printed char */
   uint8_t cursor_x, cursor_y;  /* position of the cursor, might be off screen */
-  uint8_t redraw_line;
+  uint8_t redraw_line;	/* redraw specific line if is_redraw_line is not 0 */
+  uint8_t is_redraw_line;
+  uint8_t is_redraw_all;
 };
 typedef struct u8log_struct u8log_t;
 
-uint8_t u8log_is_on_screen(u8log_t *u8log, uint8_t x, uint8_t y)
+/*
+static uint8_t u8log_is_on_screen(u8log_t *u8log, uint8_t x, uint8_t y)
 {
   if ( x >= u8log->width )
     return 0;
@@ -34,8 +42,24 @@ uint8_t u8log_is_on_screen(u8log_t *u8log, uint8_t x, uint8_t y)
     return 0;
   return 1;
 }
+*/
 
-void u8log_scroll_up(u8log_t *u8log)
+static void u8log_clear_screen(u8log_t *u8log)
+{
+  uint8_t *dest = u8log->screen_buffer;
+  uint16_t cnt = u8log->height;
+  cnt *= u8log->width;
+  do
+  {
+    *dest++ = ' ';
+    cnt--;
+  } while( cnt > 0 );
+  
+}
+
+
+/* scroll the content of the complete buffer, set redraw_line to 255 */
+static void u8log_scroll_up(u8log_t *u8log)
 {
   uint8_t *dest = u8log->screen_buffer;
   uint8_t *src = dest+u8log->width;
@@ -53,10 +77,13 @@ void u8log_scroll_up(u8log_t *u8log)
     *dest++ = ' ';
     cnt--;
   } while(cnt > 0);
-  u8log->redraw_line = 255;
+  u8log->is_redraw_all = 1;
 }
 
-void u8log_cursor_on_screen(u8log_t *u8log)
+/*
+  Place the cursor on the screen. This will also scroll, if required 
+*/
+static void u8log_cursor_on_screen(u8log_t *u8log)
 {
   if ( u8log->cursor_x >= u8log->width )
   {
@@ -70,10 +97,45 @@ void u8log_cursor_on_screen(u8log_t *u8log)
   }
 }
 
-void u8log_write_to_screen(u8log_t *u8log, uint8_t c)
+/*
+  Write a single char on the screen, do any kind of scrolling
+*/
+static void u8log_write_to_screen(u8log_t *u8log, uint8_t c)
 {
   u8log_cursor_on_screen(u8log);
   u8log->screen_buffer[u8log->cursor_y * u8log->width + u8log->cursor_x] = c;
   u8log->cursor_x++;
+  
+  if ( u8log->is_redraw_line_for_each_char )
+  {
+    u8log->is_redraw_line = 1;
+    u8log->redraw_line = u8log->cursor_y;
+  }
+}
+
+void u8log_write_char(u8log_t *u8log, uint8_t c)
+{
+  switch(c)
+  {
+    case '\n':	// 10
+      u8log->is_redraw_line = 1;
+      u8log->redraw_line = u8log->cursor_y;
+      u8log->cursor_y++;
+      u8log->cursor_x = 0;
+      break;	
+    case '\r':	// 13
+      u8log->cursor_x = 0;
+      break;
+    case '\t':	// 9
+      u8log->cursor_x = (u8log->cursor_x + 8) & 0xf8;
+      break;
+    case '\f':	// 12
+      u8log_clear_screen(u8log);
+      u8log->is_redraw_all = 1;
+      break;
+    default:
+      u8log_write_to_screen(u8log, c);
+      break;
+  }
 }
 
