bae1d87ef15675662d3a0206068e15916c910d9e
kraus <olikraus@gmail.com>
2018-04-30 23:28:03 +0200

    update

--- sys/arduino/u8g2_page_buffer/Weather/Weather.ino
+++ sys/arduino/u8g2_page_buffer/Weather/Weather.ino
@@ -325,11 +325,11 @@ void draw(const char *s, uint8_t symbol, int degree)
 void setup(void) {
 
   /* U8g2 Project: SSD1306 Test Board */
-  pinMode(10, OUTPUT);
-  pinMode(9, OUTPUT);
+  //pinMode(10, OUTPUT);
+  //pinMode(9, OUTPUT);
   
-  digitalWrite(10, 0);
-  digitalWrite(9, 0);		
+  //digitalWrite(10, 0);
+  //digitalWrite(9, 0);		
 
   /* U8g2 Project: T6963 Test Board */
   //pinMode(18, OUTPUT);
