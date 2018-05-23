1212c19364016f09696de0aee1ffb2d3536db89a
kraus <olikraus@gmail.com>
2018-05-23 21:09:34 +0200

    codebuild sh1108, #619

--- tools/codebuild/codebuild.c
+++ tools/codebuild/codebuild.c
@@ -229,6 +229,23 @@ struct controller controller_list[] =
       { NULL }
     }
   },
+
+   {
+    "sh1108", 	 20, 	20, 	"u8g2_ll_hvline_vertical_top_lsb", "u8x8_cad_001", "", COM_4WSPI|COM_3WSPI|COM_6800|COM_8080|COM_8080,
+    "Not tested", /* is_generate_u8g2_class= */ 1,
+    {
+      { "160x160" },
+      { NULL }
+    }
+  },
+  {
+    "sh1108", 	 20, 	20, 	"u8g2_ll_hvline_vertical_top_lsb", "u8x8_cad_ssd13xx_i2c", "i2c", COM_I2C,
+    "Not tested", /* is_generate_u8g2_class= */ 1,
+    {
+      { "160x160" },
+      { NULL }
+    }
+  },
   
    {
     "sh1122", 	 32, 	8, 	"u8g2_ll_hvline_horizontal_right_lsb", "u8x8_cad_001", "", COM_4WSPI|COM_3WSPI|COM_6800|COM_8080|COM_8080,
