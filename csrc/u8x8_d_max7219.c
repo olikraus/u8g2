c64f10ad6d302175393a7143a65ea1764b3c27da
kraus <olikraus@gmail.com>
2018-06-13 21:42:08 +0200

    issue #628

--- csrc/u8x8_d_max7219.c
+++ csrc/u8x8_d_max7219.c
@@ -191,7 +191,7 @@ static const u8x8_display_info_t u8x8_max7219_32x8_display_info =
   /* default_x_offset = */ 0,
   /* flipmode_x_offset = */ 0,
   /* pixel_width = */ 32,
-  /* pixel_height = */ 16
+  /* pixel_height = */ 8
 };
 
 uint8_t u8x8_d_max7219_32x8(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
@@ -203,5 +203,42 @@ uint8_t u8x8_d_max7219_32x8(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *ar
     }
     return u8x8_d_max7219_generic(u8x8, msg, arg_int, arg_ptr);
 }
+
+/*==============================*/
+
+static const u8x8_display_info_t u8x8_max7219_8x8_display_info =
+{
+  /* chip_enable_level = */ 0,
+  /* chip_disable_level = */ 1,
+  
+  /* post_chip_enable_wait_ns = */ 100,
+  /* pre_chip_disable_wait_ns = */ 100,
+  /* reset_pulse_width_ms = */ 100, 
+  /* post_reset_wait_ms = */ 100, 
+  /* sda_setup_time_ns = */ 100,	
+  /* sck_pulse_width_ns = */ 100,	
+  /* sck_clock_hz = */ 4000000UL,	/* since Arduino 1.6.0, the SPI bus speed in Hz. Should be  1000000000/sck_pulse_width_ns */
+  /* spi_mode = */ 0,		/* active high, rising edge */
+  /* i2c_bus_clock_100kHz = */ 4,
+  /* data_setup_time_ns = */ 40,
+  /* write_pulse_width_ns = */ 150,	
+  /* tile_width = */ 1,
+  /* tile_hight = */ 1,
+  /* default_x_offset = */ 0,
+  /* flipmode_x_offset = */ 0,
+  /* pixel_width = */ 8,
+  /* pixel_height = */ 8
+};
+
+uint8_t u8x8_d_max7219_8x8(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
+{
+    if ( msg == U8X8_MSG_DISPLAY_SETUP_MEMORY )
+    {
+      u8x8_d_helper_display_setup_memory(u8x8, &u8x8_max7219_8x8_display_info);
+      return 1;
+    }
+    return u8x8_d_max7219_generic(u8x8, msg, arg_int, arg_ptr);
+}
+
 
 
