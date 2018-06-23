06634fcf0f4f2bb3514c81ac2e4c307a6801dfe6
kraus <olikraus@gmail.com>
2018-06-23 08:08:52 +0200

    issue #642

--- tools/codebuild/codebuild.c
+++ tools/codebuild/codebuild.c
@@ -1001,6 +1001,14 @@ struct controller controller_list[] =
       { NULL }
     }
   },
+  {
+    "lc7981", 	30, 	8, 	"u8g2_ll_hvline_horizontal_right_lsb", "u8x8_cad_100", "", COM_6800,
+    "U8x8 not supported, no powerdown, no HW flip, no constrast", /* is_generate_u8g2_class= */ 1,
+    {
+      { "240x64" },
+      { NULL }
+    }
+  },
   
   {
     "t6963", 	30, 	16, 	"u8g2_ll_hvline_horizontal_right_lsb", "u8x8_cad_100", "", COM_8080,
