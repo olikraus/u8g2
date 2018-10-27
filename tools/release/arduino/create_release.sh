d65fc6e5b8d04749944a10e74a3ed7ad92134add
kraus <olikraus@gmail.com>
2018-10-28 00:06:09 +0200

    clip win example

--- tools/release/arduino/create_release.sh
+++ tools/release/arduino/create_release.sh
@@ -15,6 +15,9 @@ rm ./../../../../U8g2_Arduino/src/clib/u8x8_d_stdio.c
 
 # page buffer
 
+mkdir ../../../../U8g2_Arduino/examples/page_buffer/ClipWindow
+cp ../../../sys/arduino/u8g2_page_buffer/ClipWindow/*.ino ../../../../U8g2_Arduino/examples/page_buffer/ClipWindow/.
+
 mkdir ../../../../U8g2_Arduino/examples/page_buffer/StateBufferLoop
 cp ../../../sys/arduino/u8g2_page_buffer/StateBufferLoop/*.ino ../../../../U8g2_Arduino/examples/page_buffer/StateBufferLoop/.
 
