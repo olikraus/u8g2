ea809d2f3a9e103caf7006ace747e0ef8cc72fa5
kraus <olikraus@gmail.com>
2019-01-27 08:59:04 +0100

    fix flipmode mirror problem, issue #784

--- csrc/u8x8_d_ssd1318.c
+++ csrc/u8x8_d_ssd1318.c
@@ -155,7 +155,7 @@ static const uint8_t u8x8_d_ssd1318_128x96_powersave1_seq[] = {
 static const uint8_t u8x8_d_ssd1318_128x96_flip0_seq[] = {
   U8X8_START_TRANSFER(),             	/* enable chip, delay is part of the transfer start */
   U8X8_C(0x0a1),				/* segment remap a0/a1*/
-  U8X8_C(0x0c8),				/* c0: scan dir normal, c8: reverse */
+  U8X8_C(0x0c0),				/* c0: scan dir normal, c8: reverse */
   U8X8_END_TRANSFER(),             	/* disable chip */
   U8X8_END()             			/* end of sequence */
 };
@@ -163,7 +163,7 @@ static const uint8_t u8x8_d_ssd1318_128x96_flip0_seq[] = {
 static const uint8_t u8x8_d_ssd1318_128x96_flip1_seq[] = {
   U8X8_START_TRANSFER(),             	/* enable chip, delay is part of the transfer start */
   U8X8_C(0x0a0),				/* segment remap a0/a1*/
-  U8X8_C(0x0c0),				/* c0: scan dir normal, c8: reverse */
+  U8X8_C(0x0c8),				/* c0: scan dir normal, c8: reverse */
   U8X8_END_TRANSFER(),             	/* disable chip */
   U8X8_END()             			/* end of sequence */
 };
