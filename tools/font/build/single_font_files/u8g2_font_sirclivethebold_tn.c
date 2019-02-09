852e5527b6d6530a3cf3cd7b38669c0db60de363
kraus <olikraus@gmail.com>
2019-02-09 22:47:12 +0100

    new font files

---
+++ tools/font/build/single_font_files/u8g2_font_sirclivethebold_tn.c
@@ -0,0 +1,13 @@
+/*
+  Fontname: -FreeType-SirClivetheBold-Bold-R-Normal--16-160-72-72-P-79-ISO10646-1
+  Copyright: GilesBooth
+  Glyphs: 17/94
+  BBX Build Mode: 0
+*/
+const uint8_t u8g2_font_sirclivethebold_tn[162] U8G2_FONT_SECTION("u8g2_font_sirclivethebold_tn") = 
+  "\21\0\4\4\4\4\2\3\5\12\11\0\377\7\376\5\0\0\0\0\0\0\205 \5\0\262\2+\13YS"
+  "S\300@\211\2\6\2,\6\42o\2\1-\6\31[C\2.\6\22s\202\0/\13\205\323\312\220@"
+  "\361d\10\0\60\10YS\303*\252\26\61\6Rs\202\2\62\10YSC\272v\11\63\11YSC:"
+  "\61\346\26\64\11YS\203(R\353$\65\10YS\303\272t\13\66\10YS\303\272U\13\67\7YS"
+  "C:\71\70\10YS\303\252V\13\71\10YS\303\252u\13:\7QSB\230\0\0\0\0\4\377\377"
+  "\0";
