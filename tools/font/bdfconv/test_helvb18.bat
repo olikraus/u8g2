5e8960d931d41a2d3c38752f44d56839039a0402
olikraus <olikraus@gmail.com>
2020-08-30 18:06:49 +0200

    2_22 update

--- tools/font/bdfconv/test_helvb18.bat
+++ tools/font/bdfconv/test_helvb18.bat
@@ -1,3 +1,6 @@
-bdfconv.exe -v -f 1 -m "32-127" ../bdf/helvB18.bdf -o helvb18_tf.c  -n u8g2_font_helvB18_tf -d ../bdf/helvB18.bdf
+rem bdfconv.exe -v -f 1 -m "32-127" ../bdf/helvB18.bdf -o helvb18_tf.c  -n u8g2_font_helvB18_tf -d ../bdf/helvB18.bdf
+rem type helvb18_tf.c
+
+bdfconv_2_22.exe -v -f 1 -m "32-127" ../bdf/helvB18.bdf -o helvb18_tf.c  -n u8g2_font_helvB18_tf -d ../bdf/helvB18.bdf
 type helvb18_tf.c
 
