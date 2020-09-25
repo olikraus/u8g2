d1dc6abe41c3cc9ec23db407a3e0f7dd404237e1
kraus <olikraus@gmail.com>
2020-09-25 19:20:03 +0200

    issue #1291 codebuild

--- tools/codebuild/codebuild.c
+++ tools/codebuild/codebuild.c
@@ -115,6 +115,15 @@ struct controller controller_list[] =
       { NULL }
     }
   },
+
+  {
+    "ssd1306", 	255,  2, 	"u8g2_ll_hvline_vertical_top_lsb", "u8x8_cad_001", "", COM_4WSPI|COM_3WSPI|COM_6800|COM_8080,
+    "", /* is_generate_u8g2_class= */ 1,
+    {
+      { "2040x16" },
+      { NULL }
+    }
+  },
   
   {
     "ssd1306", 	16, 	8, 	"u8g2_ll_hvline_vertical_top_lsb", "u8x8_cad_001", "", COM_4WSPI|COM_3WSPI|COM_6800|COM_8080,
