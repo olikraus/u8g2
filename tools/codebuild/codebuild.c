d67363fdb1bea1ef4a8b1d0ea970c9b04e8cee72
kraus <olikraus@gmail.com>
2021-08-28 09:03:08 +0200

    issue #1565

--- tools/codebuild/codebuild.c
+++ tools/codebuild/codebuild.c
@@ -853,6 +853,24 @@ struct controller controller_list[] =
   },  
 
   {
+    "uc1609", 	24, 	8, 	"u8g2_ll_hvline_vertical_top_lsb", "u8x8_cad_001", "", COM_4WSPI|COM_3WSPI|COM_6800|COM_8080,
+    "", /* is_generate_u8g2_class= */ 1,
+    {
+      { "slg19264" },
+      { NULL }
+    }
+  },  
+
+  {
+    "uc1609", 	24, 	8, 	"u8g2_ll_hvline_vertical_top_lsb", "u8x8_cad_uc16xx_i2c", "i2c", COM_I2C,
+    "", /* is_generate_u8g2_class= */ 1,
+    {
+      { "slg19264" },
+      { NULL }
+    }
+  },  
+  
+  {
     "uc1638", 	20, 	16, 	"u8g2_ll_hvline_vertical_top_lsb", "u8x8_cad_011", "", COM_4WSPI|COM_3WSPI|COM_6800|COM_8080,
     "", /* is_generate_u8g2_class= */ 1,
     {
