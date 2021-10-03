0d8d90bae2a0a42d5e4c7dcfd2dc5ece13a3ea65
kraus <olikraus@gmail.com>
2021-10-03 11:44:16 +0200

    codebuild issue #1604

--- tools/codebuild/codebuild.c
+++ tools/codebuild/codebuild.c
@@ -1732,6 +1732,15 @@ struct controller controller_list[] =
       { NULL }
     }
   },
+
+  {
+    "gu800", 	16, 	8, 	"u8g2_ll_hvline_vertical_top_lsb", "u8x8_cad_110", "", COM_4WSPI|COM_6800|COM_8080,
+    "", /* is_generate_u8g2_class= */ 1,
+    {
+      { "128x64" },
+      { NULL }
+    }
+  },
   
   {
     "a2printer", 	48, 	30, 	"u8g2_ll_hvline_horizontal_right_lsb", "u8x8_cad_empty", "", COM_UART,
