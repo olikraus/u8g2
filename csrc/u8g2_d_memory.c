592980480e8ac92b5d70b5d6f7175d9faf8d8ad9
izilzty <izilzty@outlook.com>
2022-04-07 23:26:12 +0800

    修复GP1287AI使用动态内存分配时额外的内存占用

--- csrc/u8g2_d_memory.c
+++ csrc/u8g2_d_memory.c
@@ -1721,20 +1721,35 @@ uint8_t *u8g2_m_48_30_f(uint8_t *page_cnt)
 }
 uint8_t *u8g2_m_32_7_1(uint8_t *page_cnt)
 {
+  #ifdef U8G2_USE_DYNAMIC_ALLOC
+  *page_cnt = 1;
+  return 0;
+  #else
   static uint8_t buf[256];
   *page_cnt = 1;
   return buf;
+  #endif
 }
 uint8_t *u8g2_m_32_7_2(uint8_t *page_cnt)
 {
+  #ifdef U8G2_USE_DYNAMIC_ALLOC
+  *page_cnt = 2;
+  return 0;
+  #else
   static uint8_t buf[512];
   *page_cnt = 2;
   return buf;
+  #endif
 }
 uint8_t *u8g2_m_32_7_f(uint8_t *page_cnt)
 {
+  #ifdef U8G2_USE_DYNAMIC_ALLOC
+  *page_cnt = 7;
+  return 0;
+  #else
   static uint8_t buf[1792];
   *page_cnt = 7;
   return buf;
+  #endif
 }
 /* end of generated code */
