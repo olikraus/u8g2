73e0d93280154edc4d6b1450299144b86c2ad3d1
kraus <olikraus@gmail.com>
2022-05-26 23:08:10 +0200

    discussion #1868, issue #1869

--- tools/codebuild/codebuild.c
+++ tools/codebuild/codebuild.c
@@ -1282,6 +1282,15 @@ struct controller controller_list[] =
       { NULL }
     }
   },
+
+  {
+    "st7567", 		17, 	4, 	"u8g2_ll_hvline_vertical_top_lsb", "u8x8_cad_001", "", COM_4WSPI|COM_6800|COM_8080,
+    "", /* is_generate_u8g2_class= */ 1,
+    {
+      { "erc13232" },
+      { NULL }
+    }
+  },
   
   {
     "st7571", 		16, 	16, 	"u8g2_ll_hvline_vertical_top_lsb", "u8x8_cad_001", "", COM_4WSPI|COM_6800|COM_8080,
