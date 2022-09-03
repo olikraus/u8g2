a0226b8f193100cd73c9a0193b985bdc94189b4b
kraus <olikraus@gmail.com>
2022-09-03 15:57:09 +0200

    issue #1907 cleanup

--- sys/sdl/selection_list/main.c
+++ sys/sdl/selection_list/main.c
@@ -8,13 +8,14 @@ u8g2_t u8g2;
 int main(void)
 {
   
-  //u8g2_SetupBuffer_SDL_128x64_4(&u8g2, &u8g2_cb_r0);
-  u8g2_SetupBuffer_SDL_240x160(&u8g2, &u8g2_cb_r0);
+  u8g2_SetupBuffer_SDL_128x64_4(&u8g2, &u8g2_cb_r0);
+  //u8g2_SetupBuffer_SDL_240x160(&u8g2, &u8g2_cb_r0);
   
   u8x8_InitDisplay(u8g2_GetU8x8(&u8g2));
   u8x8_SetPowerSave(u8g2_GetU8x8(&u8g2), 0);  
 
   u8g2_SetFont(&u8g2, u8g2_font_helvB08_tr);
+  u8g2_SetFont(&u8g2, u8g2_font_4x6_tr);
   u8g2_SetFontDirection(&u8g2, 0);
   u8g2_SetFontRefHeightAll(&u8g2);
 
