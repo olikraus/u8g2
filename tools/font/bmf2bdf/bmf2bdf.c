17c063193f8761fbde64b9b0644bff7f124ca624
kraus <olikraus@gmail.com>
2022-11-20 20:47:58 +0100

    update

--- tools/font/bmf2bdf/bmf2bdf.c
+++ tools/font/bmf2bdf/bmf2bdf.c
@@ -35,18 +35,19 @@
 
   Features
     - Convert .bmf to .bdf
-    - Converts DOS codepage to Unicode
+    - Converts DOS codepage to Unicode (-u option)
     - Convert monospaced proportional font back to none-monospaced font (-p)
     - Optional glyph spacing with -x
     - Optional baseline adjustment with -y
+    - Brightness threshold with -l
     - Generated BDF can be modified with gbdfed and FontForge
       (http://sofia.nmsu.edu/~mleisher/Software/gbdfed/, https://github.com/andrewshadura/gbdfed)
     
   Example:
     http://bmf.php5.cz/index.php?font=goth16
-      bmf2bdf -p -y -2 goth16.bmf
+      bmf2bdf -u -p -y -2 goth16.bmf
     or
-      bmf2bdf -p -x 1 -y -2 goth16.bmf
+      bmf2bdf -u -p -x 1 -y -2 goth16.bmf
 
   
   
