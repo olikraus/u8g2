bd5a57efc1e36c7bb162587347713476d345926c
olikraus <olikraus@gmail.com>
2025-06-22 10:23:08 +0200

    issue #2661 codebuild

--- csrc/u8g2_d_memory.c
+++ csrc/u8g2_d_memory.c
@@ -1521,6 +1521,39 @@ uint8_t *u8g2_m_16_32_f(uint8_t *page_cnt)
   return buf;
   #endif
 }
+uint8_t *u8g2_m_25_25_1(uint8_t *page_cnt)
+{
+  #ifdef U8G2_USE_DYNAMIC_ALLOC
+  *page_cnt = 1;
+  return 0;
+  #else
+  static uint8_t buf[200];
+  *page_cnt = 1;
+  return buf;
+  #endif
+}
+uint8_t *u8g2_m_25_25_2(uint8_t *page_cnt)
+{
+  #ifdef U8G2_USE_DYNAMIC_ALLOC
+  *page_cnt = 2;
+  return 0;
+  #else
+  static uint8_t buf[400];
+  *page_cnt = 2;
+  return buf;
+  #endif
+}
+uint8_t *u8g2_m_25_25_f(uint8_t *page_cnt)
+{
+  #ifdef U8G2_USE_DYNAMIC_ALLOC
+  *page_cnt = 25;
+  return 0;
+  #else
+  static uint8_t buf[5000];
+  *page_cnt = 25;
+  return buf;
+  #endif
+}
 uint8_t *u8g2_m_48_17_1(uint8_t *page_cnt)
 {
   #ifdef U8G2_USE_DYNAMIC_ALLOC
@@ -1914,39 +1947,6 @@ uint8_t *u8g2_m_22_9_f(uint8_t *page_cnt)
   #else
   static uint8_t buf[1584];
   *page_cnt = 9;
-  return buf;
-  #endif
-}
-uint8_t *u8g2_m_25_25_1(uint8_t *page_cnt)
-{
-  #ifdef U8G2_USE_DYNAMIC_ALLOC
-  *page_cnt = 1;
-  return 0;
-  #else
-  static uint8_t buf[200];
-  *page_cnt = 1;
-  return buf;
-  #endif
-}
-uint8_t *u8g2_m_25_25_2(uint8_t *page_cnt)
-{
-  #ifdef U8G2_USE_DYNAMIC_ALLOC
-  *page_cnt = 2;
-  return 0;
-  #else
-  static uint8_t buf[400];
-  *page_cnt = 2;
-  return buf;
-  #endif
-}
-uint8_t *u8g2_m_25_25_f(uint8_t *page_cnt)
-{
-  #ifdef U8G2_USE_DYNAMIC_ALLOC
-  *page_cnt = 25;
-  return 0;
-  #else
-  static uint8_t buf[5000];
-  *page_cnt = 25;
   return buf;
   #endif
 }
